7d943b4dfc026b19eaba7a029762aafeba086612
Abdullah-Al-Sadid <abdullahalsadid1914@gmail.com>
2024-05-12 20:36:34 +0600

    initial comit

---
+++ app.js
@@ -0,0 +1,93 @@
+// var test = "Hello world";
+
+// var num = 10;
+
+// var status = true;
+
+// var obj = {
+//     name:"Sadid",
+// }
+// console.log(typeof obj);
+
+// var num = "12.5";
+// var num1 = 15;
+// var converedNum = parseFloat(num);
+
+// var result = converedNum+num1
+// console.log(result)
+// console.log(typeof result)
+
+
+// var status = "rains";
+
+// if (status=="rains"){
+//     console.log("Brishti hocce tai baire jawa jabe nah");
+// }
+// else if(status=="gorom"){
+//     console.log("Onek gorom baire jawa jabe nah");
+// }
+// else{
+//     console.log("baire jawa jabe")
+// }
+
+
+// var result = 50;
+
+// if (result<0){
+//     console.log("Failed")
+// } else if(result>=33 && result < 40){
+//     console.log("tumi C grade paicho");
+// } else if(result >=41 && result<60){
+//     console.log("tumi B grade paicho");
+// } else if (result >=61 && result<70){
+//     console.log("tumi A- Grade paicho");
+// } else {
+//     console.log("invalid");
+// }
+
+// var result = 80;
+// var person = {
+//     hand: 2,
+//     eye:2,
+//     hair:"dont know",
+//     father:{
+//         name:"jhon",
+//         age:20,
+//     },
+// }
+
+// console.log(person.father.age)
+
+var test = "Test";
+
+var friends = ["hero",5,"Alom",test,{name:"jhon"},["rohim","korim"],"jobbar"];
+// console.log(friends)
+// console.log(friends[4])
+// friends.push("Sadid")
+// friends.pop()
+// friends.unshift("Sadid")
+// friends.shift()
+// console.log(friends)
+
+// for (var i=0;i<friends.length;i++){
+//     var element = friends[i];
+//     if (element=="Alom"){
+//         console.log("Yes sir");
+//     }else{
+//         console.log("No sir");
+//     }
+// }
+
+// for (var i=0;i<20;i++){
+//      var element = i
+//      console.log(element)
+// }
+
+
+function sum(num1,num2){
+    var result = num1+num2;
+    console.log(result);
+}
+
+
+sum(20,30)
