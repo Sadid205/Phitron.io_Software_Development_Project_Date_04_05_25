c2bc79f279fa05055cb631b1ead986dd4916e782
Abdullah-Al-Sadid <abdullahalsadid1914@gmail.com>
2024-05-21 18:58:31 +0600

    initial commit

---
+++ first_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse("This is first app page.")
+
+def courses(request):
+    return HttpResponse("This is first app/course page. ")
+
+def about(request):
+    return HttpResponse("This is first app/ about page.")
---
+++ project_1/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def home(request):
+        return HttpResponse("This is home page")
+        
+def contact(request):
+
+        return HttpResponse("This is contact page")
