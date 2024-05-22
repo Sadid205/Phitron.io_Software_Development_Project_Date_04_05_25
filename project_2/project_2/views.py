23944b184ed28bffc5205d1f5925f0519621a94a
Abdullah-Al-Sadid <abdullahalsadid1914@gmail.com>
2024-05-22 16:35:11 +0600

    initial commit

---
+++ project_2/project_2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# D:\0.Phitron\Modules\Software Development Project date_04_05_24\Module 09 Introduction to Django Week-03 Date-21-05-24\project_2\templates
+def index(request):
+    return render(request,'index.html')
---
+++ project_3/first_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import datetime
+# Create your views here.
+def home(request):
+    d = {'author':'Rahim','age':20,"lst":['python','is','best'],'birthday':datetime.datetime.now(),'val' : '','courses':[
+        {
+            'id':1,
+            'name':'Python',
+            'fee': 5000
+        },
+        {
+            'id':2,
+            'name':'Django',
+            'fee':10000
+        },
+        {
+            'id':2,
+            'name':'C',
+            'fee':1000
+        },
+    ]}
+    return render(request,'first_app/home.html',d)
